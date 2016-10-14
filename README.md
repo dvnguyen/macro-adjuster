ba2ff6ddea922b09b31816a3436af2eb9c08f1e1
David Nguyen <dvnguyen@gmail.com>
2016-10-14 15:29:20 -0500

    Add Version 1: Calculate BMR

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# macro-adjuster
+# Macro Adjuster
+
+## Version 1: Requirements
+- Calculate BMR
+    - It should be able to get and set the following user metrics
+        + Age
+        + Sex
+        + Weight
+        + Height
+        + Activity level
+    - It should be able to calculate the user's BMR
+        + **Male Formula:** 66 + (6.23×Weight) + (12.7×Height) − (6.8×Age)
+        + **Women Formula:** 655 + (4.35×Wight) + (4.7×Height) − (4.7×Age)
+    - It should be able to calculate the user's adjusted BMR for their specific activity level.
---
+++ js/app.js
@@ -0,0 +1,87 @@
+var app = {
+  user: {
+    activityLevel: 3,
+    age: 33,
+    height: 72,
+    sex: 'male',
+    weight: 175
+  },
+
+  displayUserInfo: function displayUserInfo() {
+    var user = this.user;
+
+    for (var metric in user) {
+      console.log(metric.toUpperCase() + ': ' + user[metric]);
+    }
+    console.log('===================');
+    console.log('BMR: ' + this.getBMR());
+    console.log('Adjusted BMR for Level ' + this.user.activityLevel +
+     ' activity: ' + this.getAdjustedBMR(this.user.activityLevel));
+  },
+
+  getBMR: function getBMR() {
+    var bmr = null;
+
+    if (this.user.sex === 'male') {
+      bmr = 66 + (6.23 * this.user.weight) + (12.7 * this.user.height) - (6.8 * this.user.age);
+    } else if (this.user.sex === 'female') {
+      bmr = 655 + (4.35 * this.user.weight) + (4.7 * this.user.weight) - (4.7 * this.user.age);
+    }
+
+    return bmr;
+  },
+
+  getAdjustedBMR: function getAdjustedBMR(activityLevel) {
+    activityLevel = activityLevel || 0;
+
+    var bmr = this.getBMR();
+    var adjustedBMR = null;
+
+    switch(activityLevel) {
+      case 1:
+        adjustedBMR = bmr * 1.2;
+        break;
+      case 2:
+        adjustedBMR = bmr * 1.375;
+        break;
+      case 3:
+        adjustedBMR = bmr * 1.55;
+        break;
+      case 4:
+        adjustedBMR = bmr * 1.725;
+        break;
+      case 5:
+        adjustedBMR = bmr * 1.9;
+        break;
+      default:
+        return bmr;
+    }
+
+    return adjustedBMR;
+  },
+
+  setActivityLevelTo: function setActivityLevelTo(level) {
+    this.user.activityLevel = level;
+    this.displayUserInfo();
+  },
+
+  setAgeTo: function setAgeTo(age) {
+    this.user.age = age;
+    this.displayUserInfo();
+  },
+
+  setHeightTo: function setHeightTo(height) {
+    this.user.height = height;
+    this.displayUserInfo();
+  },
+
+  setSexTo: function setSexTo(sex) {
+    this.user.sex = sex;
+    this.displayUserInfo();
+  },
+
+  setWeightTo: function setWeightTo(weight) {
+    this.user.weight = weight;
+    this.displayUserInfo();
+  }
+};
